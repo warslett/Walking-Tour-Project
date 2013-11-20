319e9fcaa669fac15675ec7aa6296d1370329ace
MrYevral <hac22@aber.ac.uk>
2013-11-20 18:49:06 +0000

    adding a Link interface

---
+++ SRC/Drafts/Android/Link.java
@@ -0,0 +1,30 @@
+public interface Link{
+
+/**
+* Connects to a server with a given IP address and port number.
+*
+*
+*@param ipAddr The IP address of the server you wish to connect to.
+*@param portNum The port number of the application that you are using.
+*/
+public void connect(String ipAddr,int portNum);
+/**
+* Sends data to the server.
+*
+*
+*/
+public void send();
+
+/**
+* Receives data from the server.
+*
+*/
+public void receive();
+/**
+* Destroys the connection to the server.
+*
+*
+*/
+public void disconnect();
+
+}
