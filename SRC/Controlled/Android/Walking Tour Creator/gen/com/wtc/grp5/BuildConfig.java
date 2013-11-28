1190ab24806404fa724db631e243abc948388b14
William <williamlea@Williams-iMac.local>
2013-11-28 22:01:50 +0000

    Hold on to your pants, this is going be one hell of a commit
    Uploading the app prototype project

---
+++ SRC/Controlled/Android/Walking Tour Creator/gen/com/wtc/grp5/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.wtc.grp5;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ SRC/Controlled/Android/Walking Tour Creator/src/com/wtc/grp5/MainActivity.java
@@ -0,0 +1,49 @@
+package com.wtc.grp5;
+
+import android.os.Bundle;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements OnClickListener{
+
+	private Button btnNewWalk; 
+	private Button btnLogin;
+	private Button btnSettings;
+	private Button btnAbout;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		ActionBar ab = getActionBar();
+		ab.hide();
+		btnNewWalk = (Button) findViewById(R.id.newWalk_btn);
+		btnLogin = (Button) findViewById(R.id.logIn_btn);
+		btnSettings = (Button) findViewById(R.id.settings_btn);
+		btnAbout = (Button) findViewById(R.id.about_btn);
+		
+		// Make buttons respond to clicks
+		btnNewWalk.setOnClickListener(this);
+		btnLogin.setOnClickListener(this);
+		btnSettings.setOnClickListener(this);
+		btnAbout.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch(v.getId()){
+		case R.id.about_btn:
+			Intent intent = new Intent(this, AboutActivity.class);
+			startActivity(intent);
+			break;
+		}
+		
+	}
+
+	
+
+}
