bc22b1b34ea8096ee850b1f32a6a248a33ece926
William Lea <johnleterron@gmail.com>
2013-12-03 17:16:14 +0000

    Update Location.java

--- SRC/Controlled/Android/Location.java
+++ SRC/Controlled/Android/Location.java
@@ -1,48 +1,86 @@
 public class Location {
 
-	private long longitude;
-	private long latitude;
+	private double longitude;
+	private double latitude;
 	private String timeStamp;
-
+	
+	/**
+	 * Constructs a blank location.
+	 */
 	public Location(){
 		longitude = 0;
 		latitude = 0;
 		// timeStamp will implemented later...
 	}
-
-	public Location(long longitude, long latitude){
+	
+	/**
+	 * Constructs a location object a specified longitude and latitude.
+	 * 
+	 * @param longitude the longitude of the location.
+	 * @param latitude the latitude of the location.
+	 */
+	public Location(double longitude, double latitude){
 		this.longitude = longitude;
 		this.latitude = latitude;
 		// timeStamp will implemented later...
 	}
-
-	public void setLongitude(long longitude){
+	
+	/**
+	 * Sets a new value for this.longitude.
+	 * 
+	 * @param longitude the new value for this.longitude.
+	 */
+	public void setLongitude(double longitude){
 		this.longitude = longitude;
 	}
 
+	/**
+	 * @return the longitude of the location.
+	 */
 	public long getLongitude(){
 		return longitude;
 	}
-
-	public void setLatitude(long latitude){
+	
+	/**
+	 * Sets a new value for this.latitude.
+	 * 
+	 * @param latitude the new value for this.latitude.
+	 */
+	public void setLatitude(double latitude){
 		this.latitude = latitude;
 	}
 
+	/**
+	 * @return the latitude for the location.
+	 */
 	public long getLatitude(){
 		return latitude;
 	}
-
+	
+	/**
+	 * Sets a new value fort this.timeStamp.
+	 * 
+	 * The time stamp is set completely by the method and therefore takes no parameters.
+	 */
 	public void setTimeStamp(){
 		// Will be implemented later...
 	}
 
+	/**
+	 * @return the time stamp for when this location was recorded
+	 */
 	public String getTimeStamp(){
 		return timeStamp;
 	}
-	/*Json Generation basic layout*/
+	
+	/*
+	* Converts the data stored in this object to a JSON string.
+	* 
+	* @return the JSON String.
+	*/
 	public String toJSON(){
 		String out;
-		out = "{\"Longitude\": \""+Long.toString(longitude)+"\",\"Latitude\": \""+Long.toString(latitude)+"\"Time Stamp\": \""+timeStamp+"\"}";
+		out = "{\"Longitude\": \""+Double.toString(longitude)+"\",\"Latitude\": \""+Double.toString(latitude)+"\"Time Stamp\": \""+timeStamp+"\"}";
 		return out;
 	}
 
