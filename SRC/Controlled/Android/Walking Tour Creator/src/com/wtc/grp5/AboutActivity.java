991e1d148d29721aa14a447328aa3d6472d615cd
William <williamlea@Williams-iMac.local>
2013-12-10 22:36:41 +0000

    Okay guys we can work on the even at uni

--- SRC/Controlled/Android/Walking Tour Creator/src/com/wtc/grp5/AboutActivity.java
+++ SRC/Controlled/Android/Walking Tour Creator/src/com/wtc/grp5/AboutActivity.java
@@ -1,6 +1,7 @@
 package com.wtc.grp5;
 
 import android.os.Bundle;
+import android.widget.Toast;
 import android.app.Activity;
 
 public class AboutActivity extends Activity {
@@ -12,6 +13,7 @@ public class AboutActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_about);
+		Toast.makeText(this, "Hello", Toast.LENGTH_LONG).show();
 	}
 
 }
