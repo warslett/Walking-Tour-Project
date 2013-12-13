479e90108d0e3f6731568a407fa2c6d852c98d67
Stephen <littleclazii@live.co.uk>
2013-12-13 19:22:17 +0000

    tidied the code and added more functionality

--- SRC/Controlled/website/leaflet/leafletembed.js
+++ SRC/Controlled/website/leaflet/leafletembed.js
@@ -4,6 +4,7 @@ var plotlist;
 var plotlayers=[];
 
 function initmap() {
+
 	// set up the map
 	map = new L.Map('map');
 
@@ -15,5 +16,61 @@ function initmap() {
 	// start the map in South-East England
 	map.setView(new L.LatLng(51.3, 0.7),9);
 	map.addLayer(osm);
+	
+	// set up AJAX request
+	ajaxRequest=getXmlHttpObject();
+	if (ajaxRequest==null) {
+		alert ("This browser does not support HTTP Request");
+	return;
+	}
+	
+	askForPlots();
+	map.on('moveend', onMapMove);
 }
+
+function GetXmlHttpObject() {
+	if (window.XMLHttpRequest) { return new XMLHttpRequest(); }
+	if (window.ActiveXObject)  { return new ActiveXObject("Microsoft.XMLHTTP"); }
+	return null;
+}
+
+function askForPlots() {
+	// request the marker info with AJAX for the current bounds
+	var bounds=map.getBounds();
+	var minll=bounds.getSouthWest();
+	var maxll=bounds.getNorthEast();
+	var msg='leaflet/findbybbox.cgi?format=leaflet&bbox='+minll.lng+','+minll.lat+','+maxll.lng+','+maxll.lat;
+	ajaxRequest.onreadystatechange = stateChanged;
+	ajaxRequest.open('GET', msg, true);
+	ajaxRequest.send(null);
+}
+
+function stateChanged() {
+	// if AJAX returned a list of markers, add them to the map
+	if (ajaxRequest.readyState==4) {
+		//use the info here that was returned
+		if (ajaxRequest.status==200) {
+			plotlist=eval("(" + ajaxRequest.responseText + ")");
+			removeMarkers();
+			for (i=0;i<plotlist.length;i++) {
+				var plotll = new L.LatLng(plotlist[i].lat,plotlist[i].lon, true);
+				var plotmark = new L.Marker(plotll);
+				plotmark.data=plotlist[i];
+				map.addLayer(plotmark);
+				plotmark.bindPopup("<h3>"+plotlist[i].name+"</h3>"+plotlist[i].details);
+				plotlayers.push(plotmark);
+			}
+		}
+	}
+}
+
+function removeMarkers() {
+	for (i=0;i<plotlayers.length;i++) {
+		map.removeLayer(plotlayers[i]);
+	}
+	plotlayers=[];
+}
+
+function onMapMove(e) { 
+	askForPlots(); 
 }
