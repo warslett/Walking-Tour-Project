0f8b198f70975cf0b77ff5d3e90be4aade53ab6c
William Arslett <wia2@aber.ac.uk>
2014-01-24 12:23:26 +0000

    Formatted code for better house style.

--- SRC/Controlled/website/list.php
+++ SRC/Controlled/website/list.php
@@ -1,22 +1,47 @@
 <?
+
 include_once 'connection.php';
+
 $database = new dbConnection();
+
 function displayContent() {
+    
     global $database;
-    ?><article class="post article">
+    
+    ?>
+
+    <article class="post article">
+        
         <h2 class="postheader">List of Tours</h2>
+        
         <div class="postcontent postcontent-0 clearfix">
-            <?$list=$database->getListOfTours();
+            
+            <?
+            
+            $list=$database->getListOfTours();
+            
             foreach($list as $tour){
+                
                 print("<p>");
-                print('<a href="map.php?id=' . $tour->getID() . '">' . $tour->getTitle() . '</a><br>');
+                print('<a href="map.php?id=');
+                print($tour->getID());
+                print('">');
+                print($tour->getTitle());
+                print('</a><br>');
                 print($tour->getShortDesc());
                 print("</p>");
                 print("<hr>");
-            }?>
+                
+            }
+            
+            ?>
+            
         </div>
-    </article><?
+        
+    </article>
+
+        <?
+        
 }
 
 require_once 'template.php';
-?>
--- SRC/Controlled/website/map.php
+++ SRC/Controlled/website/map.php
@@ -1,35 +1,64 @@
 <?
+
 include_once 'connection.php';
+
 $database = new dbConnection();
+
+/* 
+ * map.php will only load if the id parameter in the get method is set.
+ * If the parameter is not set, the server will redirect to the list page.
+ */
+
 if(isset($_GET['id'])){
     $tour = $database->getTour($_GET['id']);
 } else {
     header("Location: list.php");
 }
 
+/*
+ * The header variable will be inserted into the <head> section of the page if
+ * it is declared. The following header contains scripts and stylesheets for
+ * the leaflet map.
+ */
 $header='
 <link rel="stylesheet" type="text/css" href="leaflet/leaflet.css" />
 <script type="text/javascript" src="leaflet/leaflet.js"></script>
 <script type="text/javascript" src="leaflet/leafletembed.js"></script>
 ';
+
+/*
+ * The footer will be inserted at the bottom of the page just before </body> if
+ * it is declared. The following footer contains a function to initialise the
+ * map.
+ */
 $footer='
 <script type="text/javascript">initmap()</script>
 ';
+
 function displayContent() {
+    
     global $tour;
+    
     ?>
 
     <article class="post article">
+        
         <h2 class="postheader"><? echo $tour->getTitle(); ?></h2>
+        
         <div class="postcontent postcontent-0 clearfix">
-        <? echo "<p>" . $tour->getLongDesc() . "</p>"; ?>
-           <div id="map">
-		   </div>
+            
+           <? echo "<p>" . $tour->getLongDesc() . "</p>"; ?>
+            
+           <div id="map"></div>
+           
         </div>
+        
     </article>
 
     <?
+    
 }
 
 require_once 'template.php';
+
 ?>
