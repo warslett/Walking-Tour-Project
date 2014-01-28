666a5a2a83d809bab1854ba5338f5e50a5ed63f9
William Johnathon Lea <wjl3@central.aber.ac.uk>
2014-01-28 11:23:07 +0000

    Trying to get photos to save... still

--- SRC/Controlled/Android/Walking Tour Creator/src/com/wtc/grp5/WalkActivity.java
+++ SRC/Controlled/Android/Walking Tour Creator/src/com/wtc/grp5/WalkActivity.java
@@ -44,6 +44,8 @@ public class WalkActivity extends Activity implements ConnectionCallbacks, OnCon
 	private Marker selectedMarker; // The marker the user clicked on
 	private Menu tourMenu; // A handle to the options menu for the walk
 	
+	String mCurrentPhotoPath;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -235,17 +237,47 @@ public class WalkActivity extends Activity implements ConnectionCallbacks, OnCon
 	}
 	
 	public void addPhoto() {
-		Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-		
-		
-        startActivityForResult(intent, 100);
+		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    	// Ensure that there's a camera activity to handle the intent
+    	if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+        	// Create the File where the photo should go
+        	File photoFile = null;
+        	try {
+            	photoFile = createImageFile();
+        	} catch (IOException ex) {
+            	// Error occurred while creating the File
+            
+        	}
+        	// Continue only if the File was successfully created
+        	if (photoFile != null) {
+            	takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                	    Uri.fromFile(photoFile));
+            	startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+        	}
+    	}
+	}
+	
+	private File createImageFile() throws IOException {
+    	// Create an image file name
+    	String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+    	String imageFileName = "JPEG_" + timeStamp + "_";
+    	File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+    	File image = File.createTempFile(
+    		imageFileName,  /* prefix */
+        	".jpg",         /* suffix */
+        	storageDir);    /* directory */
+
+    	// Save a file: path for use with ACTION_VIEW intents
+    	mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+    	return image;
 	}
 	
 	public void removePhoto() {
 		
 	}
 	
-
+	private File createImageFile(){
+	}
 	
 	/**
 	* Saves the tour to the server.
