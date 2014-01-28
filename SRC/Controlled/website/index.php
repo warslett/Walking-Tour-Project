5483a03fbff8a08fc973251574fcbbbcb936c32f
Stephen Clasby <littleclazii@live.co.uk>
2014-01-28 12:17:53 +0000

    initial map code input
    Task to be done before next commit:
    new CSS class and name map to mapfp
    fix the errors and sort out postitioning

--- SRC/Controlled/website/index.php
+++ SRC/Controlled/website/index.php
@@ -1,48 +1,65 @@
 <?php
-require_once 'src/template.php';
+
+include_once 'src/connection.php';
+
+$database = new dbConnection();
+
+if(isset($_GET['id'])){
+    $tour = $database->getTour($_GET['id']);
+} else {
+    header("Location: list.php");
+}
+
+$footer;
+$header='
+<link rel="stylesheet" type="text/css" href="leaflet/leaflet.css" />
+<script type="text/javascript" src="leaflet/leaflet.js"></script>
+<script type="text/javascript" src="leaflet/leafletembed.js"></script>
+';
 function displayContent() {
+    global $tour;
+    global $footer;
+    $footer='
+<script type="text/javascript">initmap(' . $tour->getID() . ')</script>
+';
 ?>
-    <div class="layout-cell sidebar1">
-        <div class="block clearfix">
-            <div class="blockheader">
-                <h3 class="t">Reasons to Travel Green</h3>
-            </div>
 
-            <div class="blockcontent" style="width: 30%">
-                <ol>
-                    <li>Vivamus bibendum nibh sit amet enim fermentum vehicula. <a href="#">more</a></li>
-
-                    <li>Cum sociis natoque penatibus et magnis dis parturient montes. <a href="#">more</a></li>
-
-                    <li>Nascetur ridiculus mus. In eget viverra libero. <a href="#">more</a></li>
-
-                    <li>Duis nisi urna, placerat id vehicula at, rhoncus vel eros. <a href="#">more</a></li>
-
-                    <li>Proin pellentesque rhoncus suscipit. <a href="#">more</a></li>
-
-                    <li>Aliquam diam tortor, dictum nec iaculis sed, dignissim et mauris. <a href="#">more</a></li>
-                </ol>
-            </div>
-        </div>
-    </div>
 
     <article class="post article">
-        <div class="postcontent postcontent-0 clearfix">
-            <div class="content-layout">
-                <div class="content-layout-row">
-                    <div class="layout-cell layout-item-1" style="width: 70%">
-                        <h3>Welcome</h3>
 
-                        <p><img alt="" class="" height="200" src="images/shutterstock_3608177.jpg" style="float:left;" width="300" /></p>
+        <div class="layout-cell sidebar1">
+            <div class="block clearfix">
+                <div class="blockheader">
+                    <h3 class="t">List of routes </h3>
+                </div>
 
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra, tellus sit amet congue vulputate, nisi erat iaculis nibh, vitae feugiat sapien ante eget mauris. Cras elit nisl, rhoncus nec iaculis ultricies, feugiat eget sapien. Pellentesque ac felis tellus.</p>
+                <div class="blockcontent">
 
-                        <p>Aenean sollicitudin imperdiet arcu, vitae dignissim est posuere id. Duis placerat justo eu nunc interdum ultrices. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-
-                        <p><a class="button" href="#">Read more</a></p>
-                    </div>
                 </div>
             </div>
+        </div>
+
+
+        <h2 class="postheader"><? echo $tour->getTitle(); ?></h2>
+
+        <div class="postcontent postcontent-0 clearfix">
+
+            <? echo "<p>" . $tour->getLongDesc() . "</p>"; ?>
+
+            <div id="map">
+
+
+
+
+
+
+
+
+
+
+            </div>
+
+        </div>
 
             <div class="content-layout-br layout-item-0"></div>
 
@@ -60,4 +77,5 @@ function displayContent() {
 
 <?php
 }
+require_once 'src/template.php';
 ?>
