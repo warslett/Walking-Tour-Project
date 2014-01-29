5a0a609311490a2b25e23abb72b67c29f4029e6a
Harvey Clark <hac22@aber.ac.uk>
2014-01-29 13:03:40 +0000

    added a poor distance calculation

--- SRC/Controlled/Android/Walking Tour Creator/src/com/wtc/grp5/model/WTCTour.java
+++ SRC/Controlled/Android/Walking Tour Creator/src/com/wtc/grp5/model/WTCTour.java
@@ -1,7 +1,7 @@
 package com.wtc.grp5.model;
 
 import java.util.LinkedList;
-
+import java.lang.Math.*
 public class WTCTour {
 
 	private String tourName;
@@ -172,5 +172,18 @@ public class WTCTour {
 			
 		}		
 	}
-
+	public long calcDist(){
+		double dist=0;
+		long lat_diff;
+		long lng_diff;
+		for(int i=1;i<locations.size();i++){
+			lat_diff = locations.get(i-1).getlatitude()-locations.get(i).getlatitude();
+			lng_diff = locations.get(i-1).getLongitude()-locations.geti().getLongitude();
+			lat_diff *= 60;
+			lng_diff *= 60;
+			dist+=Math.sqrt(Math.pow(lat_diff.doubleValue(),2)+Math.pow(lng_diff.doubleValue(),2));
+		}
+		dist = Math.round(dist);
+		return dist;		
+	}
 }
