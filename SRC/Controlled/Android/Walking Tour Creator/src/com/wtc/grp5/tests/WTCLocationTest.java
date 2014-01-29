259c32a65361aa328a5e7f574e227ed8790d2b99
William <williamlea@Williams-iMac.local>
2014-01-29 17:59:04 +0000

    Set up model unit test classes

---
+++ SRC/Controlled/Android/Walking Tour Creator/src/com/wtc/grp5/tests/WTCLocationTest.java
@@ -0,0 +1,19 @@
+package com.wtc.grp5.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class WTCLocationTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
