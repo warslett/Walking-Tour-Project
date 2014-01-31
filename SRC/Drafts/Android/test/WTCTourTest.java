c26f61c73e0b2e2c3885fb2a692b7aab418b19af
Harvey Clark <hac22@aber.ac.uk>
2014-01-31 07:27:09 +0000

    WTCTour test updated

--- SRC/Drafts/Android/test/WTCTourTest.java
+++ SRC/Drafts/Android/test/WTCTourTest.java
@@ -8,20 +8,27 @@ public class WTCTourTest {
 	@Before
 	public void setUp() throws Exception {
 		WTCTour test = new WTCTour("name","short desc","long desc");
-		WTCLocation loc1 = new WTCLocation();
-		WTCLocation loc2 = new WTCLocation();
-		WTCKeyLocation keyLoc1 = new WTCKeyLocation();
-		WTCTour.add(loc1);
-		WTCTour.add(loc2);
-		WTCTour.add(keyLoc1);
  	}
  
  	@Test
- 	public void test() {
-		//test that the gets work for each location
-		//test that the toJSON works
-		// test that get distance works
- 		fail("Not yet implemented");
- 	}
- 
- }
+ 	public void testToJSON() {
+		if(assertEquals("{\"title\": \"name\",\"longDesc\": \"long desc\",\"shortDesc\": \"short desc\",\"distance\":\"0\",\"locations\": []}",WTCTour.toJSON())){
+			System.out.println("The Strings are the same toJSON Passed");
+		}	
+		else{
+			//test that the gets work for each location
+			//test that the toJSON works
+			// test that get distance works
+ 			fail("Strings are different FAIL");
+ 		}
+	}
+	public void testAddLocation{
+	WTCLocation blank = new WTCLocation();
+	test.addLocation(WTCLocation);
+	if(assertEquals(1,WTCLocation.getLocationsSize());
+		System.out.println("Pass");
+	}
+	else{
+		fail(); 
+	}
+}
